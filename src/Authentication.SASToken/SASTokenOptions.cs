6519ed7bdcf8f2b7ae33d36f26099e7166bdc435
Michael McDaniel <mmcdaniel@gmail.com>
2024-05-29 14:35:08 -0500

    Added options to customize creation of SASToken from key

---
+++ src/Authentication.SASToken/SASTokenOptions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable enable
+namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
+{
+	/// <summary>
+	/// Options for SASToken Creation from SASTokenKey
+	/// </summary>
+	public struct SASTokenOptions
+	{
+		/// <summary>
+		/// The start when the token will be valid
+		/// </summary>
+		public DateTimeOffset? StartTime { get; set; }
+
+		/// <summary>
+		/// The absolute expiration of the token
+		/// </summary>
+		public DateTimeOffset? Expiration { get; set; }
+
+		/// <summary>
+		/// List of roles that the token can be used for
+		/// </summary>
+		public IEnumerable<string>? Roles { get; set; }
+
+		/// <summary>
+		/// The resource that this token is for
+		/// </summary>
+		public string? Resource { get; set; }
+
+		/// <summary>
+		/// a string specifies 1 or more IPAddresses in the formats: (x.x.x.x can be ipv4 or ipv6)
+		/// x.x.x.x - single IP Address
+		/// x.x.x.x/CIDR - IPAddress with CIDR Range
+		/// x.x.x.x-x.x.x.x - IPAddress range
+		/// </summary>
+		public string? AllowedIPAddresses { get; set; }
+
+		/// <summary>
+		/// Allowed protocols ex. http,https
+		/// </summary>
+		public string? Protocol { get; set; }
+	}
+}

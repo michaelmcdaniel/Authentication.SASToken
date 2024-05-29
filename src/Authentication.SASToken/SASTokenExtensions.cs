c8d1c0bffb835af96cfaf8c1a6401dc736c3d1ee
Michael McDaniel <mmcdaniel@gmail.com>
2024-05-29 14:34:03 -0500

    Added store Validation and normalized claims

--- src/Authentication.SASToken/SASTokenExtensions.cs
+++ src/Authentication.SASToken/SASTokenExtensions.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http.Extensions;
+using System.Linq;
+using System.Security.Claims;
 #nullable enable
 
 namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
@@ -32,13 +34,71 @@ namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
             if (request is null) return SASToken.Empty;
 			Microsoft.Extensions.Primitives.StringValues sv;
 			SASToken retVal;
-			if (!request.Headers.TryGetValue("Authorization", out sv) || !SASToken.TryParse(sv, out retVal))
+			if (!request.Headers.TryGetValue("Authorization", out sv) || !SASToken.TryParse(sv.ToString(), out retVal))
 			{
 				retVal = new SASToken(request.QueryString);
 			}
 			return retVal;
 		}
 
+		public static async System.Threading.Tasks.Task<bool> ValidateAsync(this ISASTokenKeyResolver store, HttpContext context, IEnumerable<string>? roles = null, string? resourceOverride = null)
+		{
+			var token = context.Request.GetSASToken();
+			var key = await store.GetAsync(token);
+			if (key is null) return false;
+			return key.Value.Validate(token, context.Request, roles, resourceOverride, context.Connection.RemoteIpAddress, context.RequestServices.GetService<ILogger<SASTokenKey>>());
+		}
+
+		public static async System.Threading.Tasks.Task<bool> ValidateAsync(this ISASTokenKeyStore store, HttpContext context, IEnumerable<string>? roles = null, string? resourceOverride = null)
+		{
+			var token = context.Request.GetSASToken();
+			var key = await store.GetAsync(token);
+			if (key is null) return false;
+			return key.Value.Validate(token, context.Request, roles, resourceOverride, context.Connection.RemoteIpAddress, context.RequestServices.GetService<ILogger<SASTokenKey>>());
+		}
+
+		/// <summary>
+		/// Produces claims set from SASToken properties
+		/// </summary>
+		/// <param name="tokenKey">The SASTokenKey for token</param>
+		/// <param name="token">The validated token</param>
+		/// <returns>Claim set</returns>
+		public static IEnumerable<Claim>? ToClaims(this SASTokenKey tokenKey, SASToken token)
+		{
+			var claims = new List<Claim>(new[] {
+					new Claim(ClaimTypes.NameIdentifier, token.Id??""),
+					new Claim(ClaimTypes.Expiration, token.Expiration.ToUnixTimeSeconds().ToString()),
+					new Claim(ClaimTypes.Uri, tokenKey.Uri.ToString()),
+					new Claim(ClaimTypes.System, token.Resource??tokenKey.Resource??""),
+					new Claim(ClaimTypes.Version, token.Version??tokenKey.Version)
+				});
+			claims.AddRange(token.Roles?.Split(',').Select(r => r.Trim()).Where(r => !string.IsNullOrEmpty(r)).Select(r => new Claim(ClaimTypes.Role, r)) ?? new Claim[0]);
+			return claims;
+		}
+
+		/// <summary>
+		/// Produces claims identity from the SASToken[Key] properties with scheme
+		/// </summary>
+		/// <param name="tokenKey">The SASTokenKey for token</param>
+		/// <param name="token">The validated token</param>
+		/// <param name="scheme">authentication scheme</param>
+		/// <returns>ClaimsIdentity</returns>
+		public static ClaimsIdentity ToClaimsIdentity(this SASTokenKey tokenKey, SASToken token, string? scheme)
+		{
+			return new ClaimsIdentity(tokenKey.ToClaims(token), scheme);
+		}
+
+		/// <summary>
+		/// Produces ClaimsPrincipal from the SASToken[Key] properties with scheme
+		/// </summary>
+		/// <param name="tokenKey">The SASTokenKey for token</param>
+		/// <param name="token">The validated token</param>
+		/// <param name="scheme">authentication scheme</param>
+		/// <returns>ClaimsPrincipal</returns>
+		public static ClaimsPrincipal ToClaimsPrincipal(this SASTokenKey tokenKey, SASToken token, string? scheme)
+		{
+			return new ClaimsPrincipal(new ClaimsIdentity(tokenKey.ToClaims(token), scheme));
+		}
 	}
 }
 

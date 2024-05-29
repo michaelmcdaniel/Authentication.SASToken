43bf5c52b441fd319da3ec4cbe0e3fd55bb44cf4
Michael McDaniel <mmcdaniel@gmail.com>
2024-05-29 14:29:02 -0500

    Added overrides for resource, ip, and scheme

--- src/Authentication.SASToken/EndpointValidator.cs
+++ src/Authentication.SASToken/EndpointValidator.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http.Extensions;
+using System.Net;
+#nullable enable
+
 namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
 {
 	/// <summary>
@@ -30,8 +33,10 @@ namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
 		/// <param name="token">the token to validate</param>
 		/// <param name="endpoint">The endpoint to check against</param>
 		/// <param name="roles">Roles to validate the token has 1 or more of.</param>
+		/// <param name="resource">Resource for token</param>
+		/// <param name="clientIP">client ip of the caller.</param>
 		/// <returns></returns>
-		public async Task<bool> IsValidAsync(SASToken token, Uri endpoint = null, IEnumerable<string> roles = null)
+		public async Task<bool> IsValidAsync(SASToken token, Uri? endpoint = null, IEnumerable<string>? roles = null, string? resource = null, IPAddress? clientIP = null)
 		{
 			SASTokenKey? tokenKey = await _store.GetAsync(token);
 			if (tokenKey == null)
@@ -39,19 +44,20 @@ namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
 				_logger.LogDebug("Token validation failed {0}: token source not found", token);
 				return false;
 			}
-			return tokenKey.Value.Validate(token, endpoint, roles, _logger);
+			return tokenKey.Value.Validate(token, endpoint, roles, resource, clientIP, endpoint?.Scheme, _logger);
 		}
 
 		/// <summary>
 		/// Validates the token against the endpoint from an httpRequest with given roles
 		/// </summary>
 		/// <param name="token">the token to validate</param>
-		/// <param name="request">The request endpoint to check against</param>
+		/// <param name="httpCtx">The http context request to validate against</param>
 		/// <param name="roles">Roles to validate the token has 1 or more of.</param>
+		/// <param name="resource">required resource</param>
 		/// <returns></returns>
-		public async Task<bool> IsValidAsync(SASToken token, Microsoft.AspNetCore.Http.HttpRequest request, IEnumerable<string> roles = null)
+		public async Task<bool> IsValidAsync(SASToken token, Microsoft.AspNetCore.Http.HttpContext? httpCtx, IEnumerable<string>? roles = null, string? resource = null)
 		{
-			return await IsValidAsync(token, new Uri(request.GetDisplayUrl()), roles);
+			return await IsValidAsync(token, new Uri(httpCtx?.Request?.GetDisplayUrl()??""), roles, resource, httpCtx?.Connection?.RemoteIpAddress);
 		}
 	}
 }

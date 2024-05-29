25197c359a01bf93e88769fe5badad47f4649663
Michael McDaniel <mmcdaniel@gmail.com>
2024-05-29 14:31:33 -0500

    added resource which force changing roles to ienumerable

--- src/Authentication.SASToken/SASTokenAuthorizationAttribute.cs
+++ src/Authentication.SASToken/SASTokenAuthorizationAttribute.cs
@@ -14,20 +14,39 @@ namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
     /// </summary>
 	public class SASTokenAuthorizationAttribute : ActionFilterAttribute, IAuthorizationFilter
 	{
-		private IEnumerable<string> _roles;
+		private IEnumerable<string> _roles = null;
+		private string _resource = null;
+		/// <summary>
+		/// Validates endpoint.
+		/// </summary>
+		public SASTokenAuthorizationAttribute()
+		{
+		}
+
 		/// <summary>
 		/// Validates endpoint and requires any of the given roles.  If no roles are given all roles will be allowed.
 		/// </summary>
 		/// <param name="roles">list of roles to require</param>
-		public SASTokenAuthorizationAttribute(params string[] roles)
+		public SASTokenAuthorizationAttribute(string[] roles)
 		{
 			_roles = roles;
 		}
 
-        /// <summary>
-        /// Returns 403 if validation fails.
-        /// </summary>
-        /// <param name="context"></param>
+		/// <summary>
+		/// Validates endpoint and requires the resource and any of the given roles.  If no roles are given all roles will be allowed.
+		/// </summary>
+		/// <param name="resource">resource required for token</param>
+		/// <param name="roles">list of roles to require</param>
+		public SASTokenAuthorizationAttribute(string resource, string[] roles)
+		{
+			_resource = resource;
+			_roles = roles;
+		}
+
+		/// <summary>
+		/// Returns 403 if validation fails.
+		/// </summary>
+		/// <param name="context"></param>
 		public void OnAuthorization(AuthorizationFilterContext context)
 		{
 			ISASTokenKeyStore tsStore = context.HttpContext.RequestServices.GetService<ISASTokenKeyStore>();
@@ -38,21 +57,14 @@ namespace mcdaniel.ws.AspNetCore.Authentication.SASToken
 			if (!(
 					!token.IsEmpty &&
 					(tokenKey = tsStore.GetAsync(token).Result).HasValue &&
-					tokenKey.Value.Validate(token, context.HttpContext.Request, _roles, loggerFactory.CreateLogger<SASTokenAuthorizationAttribute>())
+					tokenKey.Value.Validate(token, context.HttpContext.Request, _roles, _resource, context.HttpContext.Connection.RemoteIpAddress, loggerFactory.CreateLogger<SASTokenAuthorizationAttribute>())
 				))
 			{
 				context.Result = new StatusCodeResult(403);
 			}
 			else
 			{
-				var claims = new List<Claim>(new[] {
-					new Claim(ClaimTypes.NameIdentifier, token.Id.ToString()),
-					new Claim(ClaimTypes.Expiration, token.Expiration.ToUnixTimeSeconds().ToString()),
-					new Claim(ClaimTypes.Uri, tokenKey.Value.Uri.ToString()),
-					new Claim(ClaimTypes.Version, token.Version)
-				});
-                claims.AddRange(token.Roles?.Split(',').Select(r => r.Trim()).Where(r => !string.IsNullOrEmpty(r)).Select(r => new Claim(ClaimTypes.Role, r)));
-				context.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(claims, SASTokenAuthenticationDefaults.AuthenticationScheme));
+				context.HttpContext.User = tokenKey.Value.ToClaimsPrincipal(token, SASTokenAuthenticationDefaults.AuthenticationScheme);
             }
 		}
 	}

524923f82713ea8f614b9dc2677c38e3aed17bba
Michael McDaniel <mmcdaniel@gmail.com>
2024-05-29 14:38:34 -0500

    Added inline SASToken auth example

---
+++ src/examples/Authentication.SASToken.Example.Web/Controllers/From_Inline_Controller.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mcdaniel.ws.AspNetCore.Authentication.SASToken;
+
+namespace mcdaniel.ws.AspNetCore.Authentication.SASToken.Example.Web.Controllers
+{
+	[Route("api/inline")]
+	public class From_Inline_Controller(ILogger<From_Inline_Controller> _logger, ISASTokenKeyStore _tokenStore) : Controller
+	{
+
+		[HttpGet]
+		[Route("claims")]
+		[ResponseCache(Duration = 0)]
+		public async Task<IActionResult> claims() //[FromQuery(Name = "sv")] string v, [FromQuery] string sig, [FromQuery] long se, [FromQuery] string skn, [FromQuery] string? sp = null, [FromQuery] string? sip = null, [FromQuery] string? sr = null, [FromQuery] string? spr = null, [FromQuery] long st = 0)
+		{
+			if (! await _tokenStore.ValidateAsync(HttpContext)) return Forbid();
+
+			var token = HttpContext.GetSASToken();
+			var tokenKey = await _tokenStore.GetAsync(token);
+			return Json(tokenKey!.Value.ToClaims(token)!.Select(c => new Models.ClaimModel(c)) ?? new Models.ClaimModel[0]);
+		}
+
+		[HttpGet]
+		[Route("admin-claims")]
+		[ResponseCache(Duration = 0)]
+		public async Task<IActionResult> AdminClaims([FromQuery(Name = "sv")] string v, [FromQuery] string sig, [FromQuery] long se, [FromQuery] string skn, [FromQuery] string? sp = null, [FromQuery] string? sip = null, [FromQuery] string? sr = null, [FromQuery] string? spr = null, [FromQuery] long st = 0)
+		{
+			string[] anyUserInRoles = new string[] { "Admin", "PowerUsers" };
+			if (!await _tokenStore.ValidateAsync(HttpContext, anyUserInRoles)) return Forbid();
+
+			// or build it from scratch
+			var token = new SASToken()
+			{
+				Id = skn,
+				Expiration = DateTimeOffset.FromUnixTimeSeconds(se),
+				Signature = sig,
+				Roles = sp,
+				Version = v,
+				AllowedIPAddresses = sip,
+				Protocol = spr,
+				Resource = sr,
+				StartTime = st == 0 ? DateTimeOffset.MinValue : DateTimeOffset.FromUnixTimeSeconds(st)
+			};
+			var tokenKey = await _tokenStore.GetAsync(token);
+			
+			return Json(tokenKey!.Value.ToClaims(token)!.Select(c => new Models.ClaimModel(c)) ?? new Models.ClaimModel[0]);
+		}
+	}
+}
